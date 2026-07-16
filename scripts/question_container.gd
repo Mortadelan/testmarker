91f72b2b377923dcccfe5d20a25828d9c99ff7bc
mortadelan <mort@cock.li>
2026-07-15 21:55:31 -0300

    beginnings of new test maker scene for making new tests

--- scripts/question_container.gd
+++ scripts/question_container.gd
@@ -2,6 +2,7 @@ class_name QuestionContainer
 extends GridContainer
 
 @export var questions : QuestionList
+
 func _ready():
 	for questioni in questions.list.size():
 		var question := QuestionItem.new()
@@ -9,9 +10,15 @@ func _ready():
 		question.question = questions.list[questioni]
 		add_child(question)
 
-func _on_button_pressed():
+func _on_check_button_pressed():
 	for child in get_children():
 		child.check()
 
 func _on_h_slider_value_changed(value: float) -> void:
 	self.columns = int(value)
+
+func _on_set_button_pressed():
+	questions.commit_selected()
+
+func _on_save_button_pressed() -> void:
+	pass # Replace with function body.
--- scripts/question_list.gd
+++ scripts/question_list.gd
@@ -3,3 +3,8 @@ extends Resource
 
 enum OPTIONS { A, B, C, D, E }
 @export var list : Array[Question] = []
+
+func commit_selected():
+	if list.size() > 0:
+		for question in list:
+			question.correct = question.selected
